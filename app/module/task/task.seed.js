80e5f3ea9c96465793e0f34488d028a84f3304f6
Nader Toukabri <nader@emiketic.com>
2019-02-12 21:50:14 +0100

    fix test

--- app/module/task/task.seed.js
+++ app/module/task/task.seed.js
@@ -2,6 +2,8 @@
 
 const Logger = require('@/common/logger').createLogger($filepath(__filename));
 
+const CONST = require('@/common/const');
+
 const User = require('../auth/User.model');
 const Task = require('./Task.model');
 
@@ -10,14 +12,17 @@ const SharedUtils = require('@/shared/utils');
 module.exports = async () => {
   const DATA = {};
 
-  DATA.User = await User.collection.find();
+  DATA.User = await User.collection.find().where({
+    role: CONST.ROLE.CLIENT,
+  });
 
   let record;
 
   record = await Task.collection
     .create({
       title: 'Do something ...',
-      _owner: SharedUtils.getRandomArrayItem(DATA.User).id,
+      // _owner: SharedUtils.getRandomArrayItem(DATA.User).id,
+      _owner: DATA.User[0].id,
       // _manager_s: SharedUtils.getRandomArrayItemSet(DATA.User).map((item) => item.id),
     })
     .fetch();
@@ -27,7 +32,8 @@ module.exports = async () => {
   record = await Task.collection
     .create({
       title: 'Do something else ...',
-      _owner: SharedUtils.getRandomArrayItem(DATA.User).id,
+      // _owner: SharedUtils.getRandomArrayItem(DATA.User).id,
+      _owner: DATA.User[0].id,
       // _manager_s: SharedUtils.getRandomArrayItemSet(DATA.User).map((item) => item.id),
     })
     .fetch();
