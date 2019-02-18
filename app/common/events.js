704f068bfcc4df5ad42ef651d4125fd9ab2916b7
Nader Toukabri <nader@emiketic.com>
2019-02-18 15:31:07 +0100

    update starter-lib

--- app/common/events.js
+++ app/common/events.js
@@ -6,6 +6,4 @@ const EVENT = new EventEmitter();
 
 EVENT.EventEmitter = EventEmitter;
 
-EVENT.toPromise = (event) => new Promise((resolve) => EVENT.once(event, resolve));
-
 module.exports = EVENT;
