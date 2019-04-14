c80ed2952e8d104839e7e24cf7931ec9f26c1a13
Nader Toukabri <nader@emiketic.com>
2019-04-14 19:25:18 +0100

    cleanup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -xeo pipefail
 
-TARGET=$1
+DESTINATION=$1
 
-if [ -z "$TARGET" ] || [ ! -d "$TARGET" ] ; then
+if [ -z "$DESTINATION" ] || [ ! -d "$DESTINATION" ] ; then
   exit 1
 fi
 
-cd $TARGET
+cd $DESTINATION
 docker-compose pull
 docker-compose up -d
